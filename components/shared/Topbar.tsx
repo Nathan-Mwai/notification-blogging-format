335f5a221a419c7f41279510196881d135ed82a6
Nathan-Mwai <nathan.mwai@student.moringaschool.com>
2024-11-10 12:53:51 +0300

    logo works

--- components/shared/Topbar.tsx
+++ components/shared/Topbar.tsx
@@ -6,7 +6,7 @@ function Topbar() {
     // topbar from globals.css
     <nav className="topbar">
       <Link href="/" className="flex items-center gap-4">
-        <Image src="/logo.svg" alt="Medrin Logo" width={28} height={28} />
+        <Image src="/assets/Medrin.png" alt="Medrin Logo" width={28} height={28} />
         <p className="text-heading3-bold text-light-1 max-xs:hidden">
           Medrin Jobs
         </p>
