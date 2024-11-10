245d75f6b575ed4992c95c6236237e9ee6429c8a
Nathan-Mwai <nathan.mwai@student.moringaschool.com>
2024-11-10 17:21:53 +0300

    Enables nextjs to use clerk but it was a good thing that nextjs is always protecting our code from external breaches . I also adde the experimental so that nextjs can also alllow me to use mongodb as I join to backend

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,35 @@
-import type { NextConfig } from "next";
-
-const nextConfig: NextConfig = {
-  /* config options here */
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  // This is necessary for the mongodb for seeing whether project is working 
+  experimental: {
+    serverActions: true,
+    // Just making it know how to render mongoose actions for ease in mongodb work
+    serverComponentsExternalPackages: ["mongoose"],
+  },
+  // This allows us to fetch photos from clerk without running into errors i.e next.config is always protecting my code
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "img.clerk.com",
+      },
+      {
+        protocol: "https",
+        hostname: "images.clerk.dev",
+      },
+      {
+        protocol: "https",
+        hostname: "uploadthing.com",
+      },
+      {
+        protocol: "https",
+        hostname: "placehold.co",
+      },
+    ],
+    typescript: {
+      ignoreBuildErrors: true,
+    },
+  },
 };
 
-export default nextConfig;
+module.exports = nextConfig;
