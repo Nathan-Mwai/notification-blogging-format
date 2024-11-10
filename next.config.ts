f6a2cff798f8b098d6327c7a7c9a9ef045af6658
Nathan-Mwai <nathan.mwai@student.moringaschool.com>
2024-11-10 20:54:38 +0300

    Managed to resolve error dealing with importing people information from clerk

--- next.config.ts
+++ next.config.ts
@@ -1,11 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  // This is necessary for the mongodb for seeing whether project is working 
-  experimental: {
-    serverActions: true,
+  // This is necessary for the mongodb for seeing whether project is working
     // Just making it know how to render mongoose actions for ease in mongodb work
-    serverComponentsExternalPackages: ["mongoose"],
-  },
+    serverExternalPackages: ["mongoose"],
   // This allows us to fetch photos from clerk without running into errors i.e next.config is always protecting my code
   images: {
     remotePatterns: [
@@ -26,9 +23,6 @@ const nextConfig = {
         hostname: "placehold.co",
       },
     ],
-    typescript: {
-      ignoreBuildErrors: true,
-    },
   },
 };
 
