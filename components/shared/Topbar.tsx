e70cc32a5882307ec3980dfa850f3911ae34101a
Nathan-Mwai <nathan.mwai@student.moringaschool.com>
2024-11-10 12:29:40 +0300

    Added assets to my public domain

--- components/shared/Topbar.tsx
+++ components/shared/Topbar.tsx
@@ -1,5 +1,18 @@
-function Topbar(){
-    return <h1>Topbar</h1>
+import Image from "next/image";
+import Link from "next/link";
+
+function Topbar() {
+  return (
+    // topbar from globals.css
+    <nav className="topbar">
+      <Link href="/" className="flex items-center gap-4">
+        <Image src="/logo.svg" alt="Medrin Logo" width={28} height={28} />
+        <p className="text-heading3-bold text-light-1 max-xs:hidden">
+          Medrin Jobs
+        </p>
+      </Link>
+    </nav>
+  );
 }
 
-export default Topbar
+export default Topbar;
