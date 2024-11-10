73fdbf1ad79fef739974b1f23f4fba9e796a0cba
Nathan-Mwai <nathan.mwai@student.moringaschool.com>
2024-11-10 16:22:08 +0300

    Added form now checking it out

--- components/forms/AccountProfile.tsx
+++ components/forms/AccountProfile.tsx
@@ -12,7 +12,7 @@ import {
   FormMessage,
 } from "@/components/ui/form"
 import { Input } from "@/components/ui/input"
-import { Form } from "@/components/ui/form";
+import { z } from "zod"
 import { userValidation } from "@/lib/validations/user";
 
 interface Props{
@@ -37,9 +37,34 @@ const AccountProfile = ({user, btnTitle}: Props) => {
             bio: "",
         }
     })
-  return (
-    <Form>
 
+    function onSubmit(values: z.infer<typeof userValidation>) {
+        // Do something with the form values.
+        // ✅ This will be type-safe and validated.
+        console.log(values)
+      }
+
+  return (
+    <Form {...form}>
+      <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8">
+        <FormField
+          control={form.control}
+          name="username"
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel>Username</FormLabel>
+              <FormControl>
+                <Input placeholder="shadcn" {...field} />
+              </FormControl>
+              <FormDescription>
+                This is your public display name.
+              </FormDescription>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+        <Button type="submit">Submit</Button>
+      </form>
     </Form>
   )
 };
