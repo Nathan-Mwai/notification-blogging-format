564b4e531c46bbcea3a638a51f614b452685487f
Nathan-Mwai <nathan.mwai@student.moringaschool.com>
2024-11-10 23:30:21 +0300

    Backend for user is somewhat complete

--- lib/actions/user.actions.ts
+++ lib/actions/user.actions.ts
@@ -1,10 +1,41 @@
 "use server"
 
+import { revalidatePath } from "next/cache";
+import User from "../models/user.model";
 import { connectToDB } from "../mongoose"
 
 
-export async function upadateUser(): Promise<void> {
+export async function updateUser(
+    userId:string,
+    username: string,
+    name: string,
+    image: string,
+    bio: string,
+    path:string
+): Promise<void> {
     connectToDB();
 
-    
+    try {
+        await User.findOneAndUpdate({
+            id: userId
+        },
+        {
+            username: username.toLowerCase(),
+            name,
+            image,
+            bio,
+            onboarded: true
+        },{
+            upsert: true,
+            // Upsert updates and inserts
+        }
+    )
+     if(path === '/profile/edit'){
+        // Allows us to update the cache
+        revalidatePath(path)
+        // return redirect('/onboarding')
+      }
+    } catch (error) {
+        throw new Error(`Failed to create/update user: ${error.message}`)
+    }
 }
