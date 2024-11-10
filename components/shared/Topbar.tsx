4898abcb917948b2c4dab820dbe2fe242f55e732
Nathan-Mwai <nathan.mwai@student.moringaschool.com>
2024-11-10 13:30:41 +0300

    Finished on the topbar moving to the left bar

--- components/shared/Topbar.tsx
+++ components/shared/Topbar.tsx
@@ -1,3 +1,10 @@
+import {
+  ClerkProvider,
+  OrganizationProfile,
+  OrganizationSwitcher,
+  SignedIn,
+  SignOutButton,
+} from "@clerk/nextjs";
 import Image from "next/image";
 import Link from "next/link";
 
@@ -6,11 +13,47 @@ function Topbar() {
     // topbar from globals.css
     <nav className="topbar">
       <Link href="/" className="flex items-center gap-4">
-        <Image src="/assets/Medrin.png" alt="Medrin Logo" width={28} height={28} />
+        <Image
+          src="/assets/Medrin.png"
+          alt="Medrin Logo"
+          width={28}
+          height={28}
+        />
         <p className="text-heading3-bold text-light-1 max-xs:hidden">
           Medrin Jobs
         </p>
       </Link>
+
+      <div className="flex items-center gap-1">
+          <ClerkProvider>
+            <div className="block md:hidden">
+            <SignedIn>
+              <SignOutButton>
+                <div className="flex cursor-pointer">
+                  <Image
+                    src="/assets/logout.svg"
+                    alt="logout"
+                    width={24}
+                    height={24}
+                  />
+                </div>
+              </SignOutButton>
+            </SignedIn>
+      </div>
+
+
+            {/* Correct usage of OrganizationProfile with valid children */}
+
+            <OrganizationSwitcher
+              appearance={{
+                elements: {
+                //   organizationSwitcherTriggerIcon: "hidden",
+                  organizationSwitcherTrigger: "py-2 px-4",
+                },
+              }}
+            />
+          </ClerkProvider>
+        </div>
     </nav>
   );
 }
